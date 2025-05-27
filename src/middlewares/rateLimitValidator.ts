5e92668ecddbe505b9ab4495ac2b6be157f414b6
Thomas-Gambin <t.gambin.pro@gmail.com>
2025-05-27 14:17:04 +0200

    FIX: Add more request for 10sec

--- src/middlewares/rateLimitValidator.ts
+++ src/middlewares/rateLimitValidator.ts
@@ -3,7 +3,7 @@ import { rateLimit } from "express-rate-limit"
 // Add rate limiting middleware
 export const limiter = rateLimit({
     windowMs: 10 * 1000, // define number for 10 sec
-    limit: 10, // Limit each IP to 10 requests
+    limit: 50, // Limit each IP to 10 requests
     standardHeaders: "draft-8", // Use the last headers format
     legacyHeaders: false, // disable the older headers format
     message: {
